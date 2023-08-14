898382dc1d03c6911d8ce6eaff7d6cbced643359
Mukesh Rajbhandari <mukeshrajbhandari92@gmail.com>
2023-08-15 00:16:03 +0545

    login and navbar

--- website/views.py
+++ website/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+#check to see if loging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #authenticate
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"you have been loged in")
+            return redirect('home')
+        else:
+            messages.success(request,"There was an error in logging In, Please try again!!")
+            return redirect('home')
+    
+    else:
+        return render(request,'home.html', {})
+
+    
+
+def logout_user(request):
+    pass
