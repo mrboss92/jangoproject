993b1f52dec0a5b3776a117c05ba1a16c98a4a60
Mukesh Rajbhandari <mukeshrajbhandari92@gmail.com>
2023-08-19 13:29:28 +0545

    additional process

--- website/views.py
+++ website/views.py
@@ -1,28 +1,55 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
+
 def home(request):
-#check to see if loging in
+    records = Record.objects.all()
+
+
+    # check to see if loging in
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        #authenticate
-        user = authenticate(request, username = username, password = password)
+        # authenticate
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request,"you have been loged in")
+            messages.success(request, "you have been loged in")
             return redirect('home')
         else:
-            messages.success(request,"There was an error in logging In, Please try again!!")
+            messages.success(
+                request, "There was an error in logging In, Please try again!!")
             return redirect('home')
-    
-    else:
-        return render(request,'home.html', {})
 
-    
+    else:
+        return render(request, 'home.html', {'records':records})
+
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "you have been log out")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request, user)
+            messages.success(request,"you have successfully registered")
+            return redirect('home')
+        
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
